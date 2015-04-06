11743e5ac8bff999a436c8dbd327bf2015225fc0
Masayoshi Wada <developer@andantesoftware.com>
2015-04-06 23:55:11 +0900

    developmentではauto reloadする

--- app.rb
+++ app.rb
@@ -1,5 +1,10 @@
 require 'sinatra'
 require 'sinatra/json'
+require 'sinatra/reloader'
+
+configure :development do
+  register Sinatra::Reloader
+end
 
 get '/' do
   json({message: "hello, world"})
